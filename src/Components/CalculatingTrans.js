ee542201091aebc457a1792ee3a3773f20305724
Josh Burrill <joshburrill@joshs-mbp.home>
2020-03-28 12:48:07 +0000

    Added invalid date mechanism to CalculatingTrans

--- src/Components/CalculatingTrans.js
+++ src/Components/CalculatingTrans.js
@@ -13,16 +13,13 @@ export default class CalculatingTrans extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showCalculatedDates: false,
-      showCalculating: true,
-      dateRulesArray: this.props.parentDateRulesArray
+      showCalculatedDates : false,
+      showCalculating     : true,
+      dateRulesArray      : this.props.parentDateRulesArray,
+      invalidDateFound    : false,
+      invalidDatesArr     : null,
     };
   }
-
-  componentDidMount(){
-    console.log('CalculatingTrans mounted');
-
-  };
 
   onCalculationsComplete = () => {
     this.setState({ showCalculating: false, showCalculatedDates: true });
@@ -30,40 +27,45 @@ export default class CalculatingTrans extends Component {
 
   /*** Gets the updated dateRulesArray that contains the string w/ calculated dates */
   onCalculatedDates = dateRulesArray => {
-    this.setState({ dateRulesArray });
+    // Filter for any array in invalidDate key
+    const invalidDatesArr = dateRulesArray.filter(dateObj => dateObj.invalidDate !== null);
+    const invalidDateFound = invalidDatesArr[0] ? true : false;
+
+    this.setState({ dateRulesArray, invalidDateFound, invalidDatesArr });
+    console.log(this.state.invalidDatesArr);
   };
 
   mappedDates = () => {
     const currCalcDates = this.state.dateRulesArray;
     let durationIncr = 1000;
 
-    let mappedDates = currCalcDates.map((dateString, i) => {
-      durationIncr += 500;
-      return (
-        <Container style={{ paddingLeft: '0.5%', paddingRight: '0.5%'}}>
-         <Transition
-          transitionOnMount={true}
-          animation="slide up"
-          visible={true}
-          duration={durationIncr}
-          key={i}
-          
-        >
-          <Grid celled stackable centered columns={2} divided>
-            <Grid.Row>
-              <Grid.Column style={this.eventTypeStyle}>
-                {dateString.type}
-              </Grid.Column>
-              <Grid.Column style={this.dateStyle}>
-                {dateString.eventName}
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Transition>
-        </Container>
-      );
-    });
-
+      let mappedDates = currCalcDates.map((dateString, i) => {
+        durationIncr += 500;
+        return (
+          <Container style={{ paddingLeft: '0.5%', paddingRight: '0.5%'}}>
+          <Transition
+            transitionOnMount={true}
+            animation="slide up"
+            visible={true}
+            duration={durationIncr}
+            key={i}
+            
+          >
+            <Grid celled stackable centered columns={2} divided>
+              <Grid.Row>
+                <Grid.Column style={this.eventTypeStyle}>
+                  {dateString.type}
+                </Grid.Column>
+                <Grid.Column style={this.dateStyle}>
+                  {dateString.eventName}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Transition>
+          </Container>
+        );
+      });
+    
     return mappedDates;
   }
 
@@ -89,7 +91,14 @@ export default class CalculatingTrans extends Component {
     if (this.state.showCalculatedDates) {
       return (
         <div className="ui one column centered grid">
-          <div className="column">{this.mappedDates()}</div>
+          <div className="column">
+            {console.log(`Invalid date found: ${ new Date(this.state.invalidDatesArr[0].invalidDate[1])}`)}
+            {console.log(`Previous date found: ${ new Date(this.state.invalidDatesArr[0].invalidDate[0])}`)}
+            {console.log(`Future date found: ${ new Date(this.state.invalidDatesArr[0].invalidDate[2])}`)}
+
+            {this.mappedDates()}
+            
+          </div>
         </div>
       );
 
